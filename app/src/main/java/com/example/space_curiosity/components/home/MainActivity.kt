9f6d3b005658481ba88ed10b2d570be68ac8fb43
Mauro <mauroalderete.10@gmail.com>
2021-11-27 01:53:05 -0300

    base complete

--- app/src/main/java/com/example/space_curiosity/components/home/MainActivity.kt
+++ app/src/main/java/com/example/space_curiosity/components/home/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.space_curiosity.R
 import com.example.space_curiosity.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+
     val vm : MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +20,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        vm.getInfoNasa()
 
         vm.responseInfo.observe(this,{
-            //terminado, completar parte IU
+            binding.tvTitulo.setText(it.title)
+            binding.tvFecha.setText(it.date)
+            binding.tvCopy.setText(it.copyright)
+            Picasso.get().load(it.hdurl).into(binding.imageView2)
+            binding.tvExplained.setText(it.explanation)
         })
 
     }
